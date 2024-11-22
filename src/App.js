5ce16c0f196395c57bdc21502a8c3414df6d38eb
urkkiz225 <urkkiz225@gmail.com>
2024-11-23 00:58:16 +0200

    npm added react95 and its components + simple window

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { styleReset, Window, WindowHeader, WindowContent, Button, Toolbar } from 'react95';
+import { Computer, Folder, Notepad, Camera as Eye, Close } from '@react95/icons';
+import original from 'react95/dist/themes/original';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={original}>
+      <div className="App">
+        <style>{styleReset}</style>
+        <Window style={{ width: 300, margin: 'auto', marginTop: '50px' }}>
+          <WindowHeader>
+            <span>
+              <Computer style={{ marginRight: '8px' }} />
+              Main window
+            </span>
+            <Button style={{ float: 'right' }}>
+              <Close />
+            </Button>
+          </WindowHeader>
+          <Toolbar>
+            <Button variant="menu">
+              <Folder style={{ marginRight: '4px' }} />
+              File
+            </Button>
+            <Button variant="menu">
+              <Notepad style={{ marginRight: '4px' }} />
+              Edit
+            </Button>
+            <Button variant="menu">
+              <Eye style={{ marginRight: '4px' }} />
+              View
+            </Button>
+          </Toolbar>
+          <WindowContent>
+            <p>
+              <Computer style={{ marginRight: '8px' }} />
+              Does this shit even work?
+            </p>
+          </WindowContent>
+        </Window>
+      </div>
+    </ThemeProvider>
   );
 }
 

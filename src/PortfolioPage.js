9221bf155249f5139697cc006408c2a013c31cb8
urkkiz225 <urkkiz225@gmail.com>
2025-01-10 16:53:38 +0200

    portfolio page

--- src/PortfolioPage.js
+++ src/PortfolioPage.js
@@ -4,22 +4,45 @@ import { styleReset } from 'react95';
 import modernDark from 'react95/dist/themes/redWine';
 import AppBarComponent from './AppBarComponent';
 import WindowComponentDescription from './WindowComponentDescription';
+import WindowComponentProject from './WindowComponentProject';
 import './style.css';
 import cat from './cat.png';
+import Kesasim from './ProjectHeaders/Kesasim.png';
+import BRUTALMANIA from './ProjectHeaders/BRUTALMANIA.jpg';
+import WindowComponent from './WindowComponent';
 
 const MainPage = () => {
     return (
         <ThemeProvider theme={modernDark}>
           <div className="App" id="App">
             <style>{styleReset}</style>
-            <div style={{ paddingTop: '50px' }}>
-              <WindowComponentDescription
-                title="About me"
-                content="fake"
-                posX={20}
-                posY={20}
-                height={200}
+            <div style={{ paddingTop: '50px' }} className = {'.App'}>
+              <WindowComponentProject
+                title = "Brutalmania"
+                content = "THIS! IS! BRUTALMANIA!!!"
+                contentSide = "HAIL BRUTE! Welcome to Brutalmania! AN endless fight for life, gold and splendor at the bloody arena. Show your enemies your strength and ruhtlessness. Have no mercy! Dare to reach for power! Collect gold, improve yourself and your gear, fight again and again!"
+                width = {500}
+                height = {500}
+                posX = {20}
+                posY = {30}
+                img = {BRUTALMANIA}
+                imgWidth={320}
+                imgHeight={280}
+                githubLink={"https://brutalmania.io/?v=1.2.63"}
               />
+              <WindowComponentProject
+                title = "Kesäsimulaattori"
+                content = "An ages old project, made in Unity 2020.1.1f1, HDRP utilized and C# typed all the way through. Contains light shaderwork in the mix."
+                contentSideRight = "A certified Finnish summer experience - down to the finite details. A passion project for a while - before the inevitablility of it all pushed towards new winds."
+                width = {500}
+                height = {400}
+                posX = {40}
+                posY = {40}
+                img = {Kesasim}
+                imgwidth = {320}
+                imgHeight={180}
+                githubLink={"https://github.com/urkkiz225/kesasimulaattori"}
+                WindowComponentProject/>
               <img src={cat} alt='cat' className="center-image" />
             </div>
             <AppBarComponent />
--- src/WindowComponentProject.js
+++ src/WindowComponentProject.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import Draggable from 'react-draggable';
 import { Window, WindowHeader, WindowContent, Button, Toolbar } from 'react95';
-import {Computer} from '@react95/icons';
+import {Computer, Right} from '@react95/icons';
 
-const WindowComponentProject = ({ title, content, contentSide, img, icon, width, height, imgWidth, imgHeight, imgScale, posX, posY, githubLink }) => {
+const WindowComponentProject = ({ title, content, contentSideLeft, contentSideRight, img, icon, width, height, imgWidth, imgHeight, imgScale, posX, posY, githubLink, mainScale }) => {
   const [defaultPosition, setDefaultPosition] = useState({ x: posX, y: posY });
 
   useEffect(() => {
@@ -27,9 +27,9 @@ const WindowComponentProject = ({ title, content, contentSide, img, icon, width,
 
   return (
     <Draggable handle=".window-header" defaultPosition={defaultPosition}>
-      <div style = {{overflow:'hidden'}}>
+      <div>
         <span className="ms-sans-serif">
-          <Window style={{ width: width, height: height, margin: 'auto', marginTop: 200, overflow: 'hidden'}}>
+          <Window style={{ width: width, height: height, margin: 'auto', marginTop: 200, overflowX: 'hidden', scrollbarColor: 'red', transform: `scale(${mainScale})` }}>
             <WindowHeader className="window-header">
                 {icon ? (
                   <img src={icon} alt="icon" style={{ marginRight: '8px', width: '16px', height: '16px' }} />
@@ -40,7 +40,8 @@ const WindowComponentProject = ({ title, content, contentSide, img, icon, width,
             </WindowHeader>
             <WindowContent style={{ display: 'flex', flexDirection: 'column', padding: '10px', overflow: 'hidden' }}>
               <div style={{ flex: '1 1 auto', overflow: 'auto', width: '100%' }}>
-                <div style={{ display: 'flex', justifyContent: "space-evenly", alignItems: 'center', width: '100%'}}>
+                <div style={{ display: 'flex', justifyContent: "space-evenly", alignItems: 'center', width: '100%', marginLeft: '10px', marginLeft : '10px'}}>
+                  {<p>{contentSideLeft}</p>}
                   <img
                     src={img}
                     alt="alt"
@@ -52,7 +53,7 @@ const WindowComponentProject = ({ title, content, contentSide, img, icon, width,
                       margin: '20px 0'
                     }}
                   />
-                  {<p>{contentSide}</p>}
+                  {<p>{contentSideRight}</p>}
                 </div>
                 <p style = {{overflow: 'hidden'}}>{content}</p>
                 <Button variant = "github" size = 'md' padding = '20px' style = {{overflow: 'hidden'}} onClick = {openLink}>
